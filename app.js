100bbee82f2cf1b172d2ea4b21d9aa4661626ac4
Marly <marlenak90@gmail.com>
2020-09-30 17:06:12 +0200

    authentication logic done

--- app.js
+++ app.js
@@ -1,52 +1,89 @@
 require("dotenv").config();
 const express = require("express");
+const logger = require('morgan');
 const path = require("path");
-const endpoints = require("./routes/endpoints");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const favicon = require('serve-favicon');
+const mongoose = require('mongoose');
 const app = express();
+const session = require('express-session');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const endpoints = require("./routes");
+const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/User');
 const Trip = require('./models/Trip');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
 
-app.use("/api", endpoints);
-passport.serializeUser((user, done) => {
-    done(null, user._id);
+require('./configs/passport.js');
+
+
+mongoose
+  .connect('mongodb://localhost/yohoho', { useNewUrlParser: true })
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
   });
-  
-  passport.deserializeUser((id, done) => {
-    User.findById(id)
-      .then(dbUser => {
-        done(null, dbUser);
-      })
-      .catch(error => {
-        done(error);
-      });
-  });
-  
-  passport.use(
-    new LocalStrategy((username, password, done) => {
-      User.findOne({ username: username })
-        .then(found => {
-          if (found === null) {
-            done(null, false, { message: 'Wrong Credentials' });
-          } else if (!bcrypt.compareSync(password, found.password)) {
-            done(null, false, { message: 'Wrong Credentials' });
-          } else {
-            done(null, found);
-          }
-        })
-        .catch(error => {
-          done(error, false);
-        });
+
+const MongoStore = require('connect-mongo')(session);
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 },
+    saveUninitialized: false,
+    resave: true,
+    store: new MongoStore({
+      // when the session cookie has an expiration date
+      // connect-mongo will use it, otherwise it will create a new 
+      // one and use ttl - time to live - in that case one day
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 * 1000
     })
-  );
-  
-  app.use(passport.initialize());
-  app.use(passport.session());
-  
-  
-// If no routes match, send them the React HTML.
-app.use(express.static(path.join(__dirname, "./frontend/build")));
+  })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express View engine setup
+
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  sourceMap: true
+}));
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+
+
+// default value for title local
+app.locals.title = 'Express - Generated with IronGenerator';
+
+
+
+const index = require('./routes/index');
+app.use('/', index);
+
+const auth = require('./routes/auth');
+app.use('/api/auth', auth);
 
 module.exports = app;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from 'react-router-dom';
 import App from "./App";
+import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+axios.get('/api/auth/loggedin')
+    .then(response => {
+        const user = response.data;
+        ReactDOM.render(
+            <BrowserRouter>
+                <App user={user} />
+            </BrowserRouter>,
+            document.getElementById('root')
+        );
+    });
+
+serviceWorker.unregister();
