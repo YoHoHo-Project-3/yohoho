1ab8f06a4ab72f84f519b0de753441fd6ff3bdd7
Your Name <daniela.kirilova@gmx.de>
2020-10-01 17:21:07 +0200

    home-page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Home from "./pages/Home";
-import About from "./pages/About";
+ 
 import Signup from './components/Signup';
 import Login from './components/Login';
-
+import Navbar from './components/Navbar'
 
 class App extends Component {
 
@@ -23,9 +23,10 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
+      <Navbar/> 
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
+         
         </Switch>
         <Route
           exact
