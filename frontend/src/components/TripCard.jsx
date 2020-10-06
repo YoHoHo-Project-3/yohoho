6bacaf5345c830c25e3cf4f4a96d1d4692ddda91
Your Name <daniela.kirilova@gmx.de>
2020-10-06 13:47:18 +0200

    changes on chat

--- frontend/src/components/TripCard.jsx
+++ frontend/src/components/TripCard.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
 import "./TripCard.css";
 const TripCard = ({ trip }) => {
   return (
     <div>
       <Card style={{ width: "17rem", backgroundColor: "#ffffff" }}>
-        <Card.Img variant="top"   src={trip.image} />
+        <Card.Img variant="top" src={trip.image} />
         <Card.Body>
-          <Card.Title>Title:  {trip.title}</Card.Title>
+          <Card.Title>Title: {trip.title}</Card.Title>
           <Card.Text>Description: {trip.description}</Card.Text>
           <Card.Text>Start: {trip.locationStart}</Card.Text>
           <Card.Text>End: {trip.locationEnd}</Card.Text>
@@ -16,11 +16,14 @@ const TripCard = ({ trip }) => {
           <Card.Text>Name of boat: {trip.boatName}</Card.Text>
           <Card.Text>Price: {trip.price}</Card.Text>
           <Card.Text>Available slots: {trip.slotsAvailable}</Card.Text>
-          <Card.Text><Link to={'/profile/'+trip.user}>Boat Owner Profile</Link></Card.Text>
+          <Card.Text>
+            <Link to={"/profile/" + trip.user}>Boat Owner Profile</Link>
+          </Card.Text>
         </Card.Body>
         <button>Book now</button>
+        <button>Delete</button>
+        <Link to="/chat">Chat</Link>
       </Card>
-   
     </div>
   );
 };
