1b373c647e91a3f9ac25d7cbe7570dca6e3be4d9
Your Name <daniela.kirilova@gmx.de>
2020-10-08 14:50:20 +0200

    build frontend

---
+++ frontend/build/precache-manifest.245dda5dc2d0c6b7f1e989a6844b963a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60907bebdcced9a175bacae5cce6ac2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e32b31f278cb8125bffa",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "cd263f10580c5c027306",
+    "url": "/static/css/main.c9f8e327.chunk.css"
+  },
+  {
+    "revision": "e32b31f278cb8125bffa",
+    "url": "/static/js/2.8e319381.chunk.js"
+  },
+  {
+    "revision": "df958f16b07871c3679510165e97a61a",
+    "url": "/static/js/2.8e319381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd263f10580c5c027306",
+    "url": "/static/js/main.7c28cd03.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "6832a7fd0937a2afe9c43e8287cc780d",
+    "url": "/static/media/boat.6832a7fd.jpeg"
+  },
+  {
+    "revision": "46425fc8ec3afd63c5593c1d50838827",
+    "url": "/static/media/boat1.46425fc8.jpeg"
+  },
+  {
+    "revision": "6aafe2a8088146934f4a2a47caf0f423",
+    "url": "/static/media/sailboat-2423484_1920.6aafe2a8.jpg"
+  }
+]);
